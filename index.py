3186803da5783795549a965763364befbaf2ccd6
CkMadhavan <madhavan.champion@gmail.com>
2021-08-19 18:31:22 +0530

    Initial

---
+++ index.py
@@ -0,0 +1,17 @@
+from firebase import firebase
+from flask import Flask , render_template
+
+firebase_app = firebase.FirebaseApplication('https://algodoc-d9ccc-default-rtdb.firebaseio.com/', None)
+app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),  'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/')
+def index():
+    result = firebase_app.get('/AlgoDocURL', None)
+    return render_template('hello.html' , algourl = result)
+
+if __name__ == '__main__':
+    app.run()
