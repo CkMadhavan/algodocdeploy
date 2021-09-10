5efaba6bc2a32ef47096e09ed7ef154a705ee7c8
CK Madhavan <33689397+CkMadhavan@users.noreply.github.com>
2021-09-10 15:57:32 +0530

    Update index.py

--- index.py
+++ index.py
@@ -7,19 +7,19 @@ firebase_project_url = os.environ.get('FirebaseProjectUrl')
 firebase_app = firebase.FirebaseApplication(firebase_project_url, None)
 app = Flask(__name__)
 
+result = firebase_app.get('/AlgoDocURL', None)
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),  'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def index():
-    result = firebase_app.get('/AlgoDocURL', None)
     return render_template('hello.html' , algourl = result)
 
 @app.route('/<something>')
 def other(something):
-    result = firebase_app.get('/AlgoDocURL', None) + '/' + something
-    print(result)
+    result = result + '/' + something
     return render_template('hello.html' , algourl = result)
 
 if __name__ == '__main__':
