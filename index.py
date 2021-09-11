0912eecfae2345a97d8afb93748ab8182b3dc50f
CK Madhavan <33689397+CkMadhavan@users.noreply.github.com>
2021-09-11 23:45:42 +0530

    Update index.py

--- index.py
+++ index.py
@@ -8,6 +8,10 @@ firebase_project_url = os.environ.get('FirebaseProjectUrl')
 firebase_app = firebase.FirebaseApplication(firebase_project_url, None)
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def not_found(e):
+    return 'hehe boi'
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),  'favicon.ico', mimetype='image/vnd.microsoft.icon')
@@ -36,6 +40,19 @@ def other(something):
         return render_template('undermaintenance.html')
     else:
         return render_template('hello.html' , algourl = result)
+    
+@app.route('/v/<some>')
+def otherv(some):
+
+    oriurl = firebase_app.get('/AlgoDocURL', None)
+    result = oriurl + '/v/' + some
+    
+    abcd = requests.get(oriurl).text
+    
+    if ('.ngrok.io not found' in abcd) or ('Failed to complete tunnel connection' in abcd):
+        return render_template('undermaintenance.html')
+    else:
+        return render_template('hello.html' , algourl = result) 
 
 if __name__ == '__main__':
     app.run()
